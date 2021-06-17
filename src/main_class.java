18895f08a3081ec048923f1ff197c4bc60ab8ab6
Jacob Lammert <son.of.archilles@gmail.com>
2021-06-17 11:23:19 +0200

    Added Apache Commons CLI parser

--- src/main_class.java
+++ src/main_class.java
@@ -1,12 +1,14 @@
+import org.apache.commons.cli.*;
+
 import java.util.Arrays;
 
 public class main_class {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println(Arrays.toString(args));
 
         // example:
-        // test --in inppaa --out outaa --invers 1234 --outvers 1235
+        // --in inputfile --out outputfile --invers 1234 --outvers 1235
 
         String input_file = "", output_file = "";
         String input_version = "", output_version = "";
@@ -14,42 +16,38 @@ public class main_class {
 
 
         try {
-            name = args[0];
-        }catch (Exception e){
+            Options options = new Options();
+            Option in = Option.builder().longOpt("in").argName("file").hasArg().desc("use given file for log").build();
+            Option out = Option.builder().longOpt("out").argName("file").hasArg().desc("use given file for log").build();
+            Option invers = Option.builder().longOpt("invers").argName("file").hasArg().desc("use given file for log").build();
+            Option outvers = Option.builder().longOpt("outvers").argName("file").hasArg().desc("use given file for log").build();
+
+            options.addOption(in);
+            options.addOption(out);
+            options.addOption(invers);
+            options.addOption(outvers);
+            CommandLineParser parser = new DefaultParser();
+            CommandLine cmd = parser.parse(options, args);
+
+            // has the logFile argument been passed?
+            if (cmd.hasOption("in")) {
+                //get the logFile argument passed
+                System.out.println(cmd.getOptionValue("in"));
+                input_file = cmd.getOptionValue("in");
+            }if (cmd.hasOption("out")) {
+                System.out.println(cmd.getOptionValue("out"));
+                output_file = cmd.getOptionValue("out");
+            }if (cmd.hasOption("invers")) {
+                System.out.println(cmd.getOptionValue("invers"));
+                input_version = cmd.getOptionValue("invers");
+            }if (cmd.hasOption("outvers")) {
+                System.out.println(cmd.getOptionValue("outvers"));
+                output_version = cmd.getOptionValue("outvers");
+            }
+        } catch (Exception e) {
             System.out.println(e);
         }
 
-        for (int i = 0; i < args.length; ++i){
-            if (args[i].contains("--")){
-                String argument = args[i].replace("--","");
-
-                switch (argument) {
-
-                    case("in"): {
-                        input_file = args[i + 1].toString();
-                    }
-                    case ("out"): {
-                        output_file = args[i + 1].toString();
-
-                    }
-                    case("invers"): {
-                        // version number of the input
-                        input_version = args[i + 1].toString();
-
-                    }
-                    case ("outvers"): {
-                        // version number of the output
-                        output_version = args[i + 1].toString();
-
-                    }
-                    break;
-                    default:
-                        throw new IllegalStateException("Unexpected value: " + argument + "\nShould be: test --in inppaa --out outaa --invers 1234 --outvers 1235");
-                }
-            }
-        }
-
-        System.out.println(name);
         System.out.println("Input file: " + input_file);
         System.out.println("Output file: " + output_file);
         System.out.println("Input version: " + input_version);
